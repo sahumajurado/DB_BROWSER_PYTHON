500a7dd2e094642e5bc02870e60fd2baabe310ff
sahumajurado <2113110059@untels.edu.pe>
2023-10-11 12:08:20 -0500

    Conexión de la base de datos con DBrowser y Python

---
+++ history.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Wed Oct 11 09:11:44 2023)---
+runfile('C:/Users/Alumno/.spyder-py3/temp.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/Desktop/prueb.py', wdir='C:/Users/Alumno/Desktop')
+
+## ---(Wed Oct 11 09:16:30 2023)---
+runcell(0, 'C:/Users/Alumno/Desktop/prueb.py')
+runfile('C:/Users/Alumno/Desktop/prueb.py', wdir='C:/Users/Alumno/Desktop')
+
+## ---(Wed Oct 11 09:17:31 2023)---
+runcell(0, 'C:/Users/Alumno/Desktop/prueb.py')
+runfile('C:/Users/Alumno/Desktop/prueb.py', wdir='C:/Users/Alumno/Desktop')
+runcell(0, 'C:/Users/Alumno/Desktop/prueb.py')
+runfile('C:/Users/Alumno/Desktop/prueb.py', wdir='C:/Users/Alumno/Desktop')
+runcell(0, 'C:/Users/Alumno/Desktop/prueb.py')
+runfile('C:/Users/Alumno/Desktop/prueb.py', wdir='C:/Users/Alumno/Desktop')
+runcell(0, 'C:/Users/Alumno/Desktop/prueb.py')
+runfile('C:/Users/Alumno/Desktop/prueb.py', wdir='C:/Users/Alumno/Desktop')
+runfile('C:/Users/Alumno/.spyder-py3/leer_archivo.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/grabar_archivo.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/agregar_datos_archivo.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/eliminar_archivo.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/gestion_archivos.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/aplicaciones.py', wdir='C:/Users/Alumno/.spyder-py3')
+
+## ---(Wed Oct 11 11:29:50 2023)---
+runfile('C:/Users/Alumno/.spyder-py3/untitled0.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/crearbasededatos.py', wdir='C:/Users/Alumno/.spyder-py3')
+
+## ---(Wed Oct 11 11:40:33 2023)---
+runfile('C:/Users/Alumno/.spyder-py3/crearbasededatos.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/insertar_registros.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/crearbasededatos.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/insertar_registros.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/listar_biblioteca.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/eliminar_registro.py', wdir='C:/Users/Alumno/.spyder-py3')
+runfile('C:/Users/Alumno/.spyder-py3/modificar_tabla.py', wdir='C:/Users/Alumno/.spyder-py3')
